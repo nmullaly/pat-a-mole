30b7a5bd9b1eb7bb102a7760ad5d10fa144b34a1
Nathan Mullaly <98053115+nmullaly@users.noreply.github.com>
2023-03-09 19:22:47 -0500

    Add files via upload

---
+++ pat a mole/app.js
@@ -0,0 +1,58 @@
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const pat = document.querySelector('.pat')
+const timeLeft = document.querySelector('#time-left')
+const score = document.querySelector('#score')
+
+let result = 0
+let hitPosition
+let currentTime = 60
+let timerId = null
+
+function randomSquare() {
+  squares.forEach(square => {
+    square.classList.remove('mole')
+    square.classList.remove('pat')
+  })
+
+  let randomSquare = squares[Math.floor(Math.random() * 9)]
+  randomSquare.classList.add('mole')
+
+  hitPosition = randomSquare.id
+}
+
+squares.forEach(square => {
+  square.addEventListener('mousedown', () => {
+    if (square.id == hitPosition) {
+      square.classList.remove('mole')
+      square.classList.add('pat')
+      // let patTimer = null
+      // patTimer = setInterval(() => {
+      //   square.classList.remove('pat')
+      // }, 1000)
+      result++
+      score.textContent = result
+      hitPosition = null
+    }
+  })
+})
+
+function moveMole() {
+  timerId = setInterval(randomSquare, 1000)
+}
+
+moveMole()
+
+function countDown() {
+  currentTime--
+  timeLeft.textContent = currentTime
+
+  if (currentTime == -1) {
+    timeLeft.textContent = '0'
+    clearInterval(countDownTimerId)
+    clearInterval(timerId)
+    alert('GAME OVER! Your final score is ' + result)
+  }
+}
+
+let countDownTimerId = setInterval(countDown, 1000)
